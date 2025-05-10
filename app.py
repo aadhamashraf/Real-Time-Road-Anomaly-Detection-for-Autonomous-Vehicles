f0fe318d0144e52558df2b0645e03ecf0c787014
ZiadMAlsawy <s-ziad.sawy@zewailcity.edu.eg>
2025-05-10 21:08:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -16,18 +16,20 @@ app.config['PROCESSED_FOLDER'] = 'processed/'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['PROCESSED_FOLDER'], exist_ok=True)
 class_names = ['longitudinal_crack', 'transverse_crack', 'alligator_crack', 'pothole', 'other_damage']
-VGG16Model = load_model("Models/best_modelVGG19_fixed5.keras")
-YoloModel = YOLO("Models/yolov8n.pt")   # This Will Be Changed
+VGG16Model = load_model("Models/best_vgg16FineTuned.keras")
+YoloModel = YOLO("Models/yolov8n.pt")  
 ResNet50Model = load_model("Models/best_modelVGG19_fixed5.keras")# This Will be Changed
-EfficientNetB3Model = load_model("Models/best_modelVGG19_fixed5.keras")# This Will be Changed
+EfficientNetB3Model = load_model("Models/best_efficientnetb3.keras")
 EfficientNetV2LModel = load_model("Models/best_modelVGG19_fixed5.keras")# This Will be Changed
-VGG19Model = load_model("Models/best_modelVGG19_fixed5.keras")# This Will be Changed
+VGG19Model = load_model("Models/best_modelVGG19_fixed5.keras")
+DenseNetModel = load_model("Models/best_densenet.keras")
 models = {"VGG16":VGG16Model,
           "YOLOv8":YoloModel,
           "ResNet50":ResNet50Model,
           "VGG19":VGG19Model,
           "EfficientNetV2L":EfficientNetV2LModel,
-          "EfficientNetB3":EfficientNetB3Model}
+          "EfficientNetB3":EfficientNetB3Model,
+          "DenseNet201":DenseNetModel}
 name = ""
 @app.route('/')
 def index():
@@ -68,21 +70,55 @@ def get_processed_video(filename):
 
 def process_image(input_path, output_path, checked_value):
     image = cv2.imread(input_path)
-    
+    present_classes = []
     resized_image = cv2.resize(image, (224, 224))
     predicted_class = ""
     img_array = np.expand_dims(resized_image, axis=0)  
     img_array = img_array / 255.0  
     if(checked_value != "YOLOv8"):
+        if(checked_value == "VGG16"  or checked_value == "DenseNet201" or checked_value == "EfficientNetB3"):
+            print("L")
+            prediction = models[checked_value].predict(img_array)
+            print(prediction)
+            prediction = prediction[0]  
+            print(prediction)
+
+            binary_prediction = (prediction > 0.4).astype(int)
+
+            print(binary_prediction)
+
+            present_classes.append([class_names[i] for i in range(5) if binary_prediction[i] == 1])
+
+            print(f"Detected classes: {present_classes}")
+            y_offset = 30  
+
+            for i in present_classes[0]:
+                print(i)
+                cv2.putText(image, i, (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
+                y_offset += 30  
     
     
-        prediction = models[checked_value].predict(img_array)
-        predicted_class_idx = np.argmax(prediction, axis=1)[0]
-        i = predicted_class_idx[0]
-        l = np.argmax(np.bincount(i))
-        predicted_class = class_names[l]
+        if(checked_value =="VGG19"):
+            prediction = models[checked_value].predict(img_array)
+
+            prediction = prediction[0]  
+
+            binary_prediction = (prediction > 0.5).astype(int)
+
+            print(binary_prediction)
+
+            present_classes = [class_names[i] for i in range(len(binary_prediction)) if binary_prediction[:, :, i].any()]
+
+            print(f"Detected classes: {present_classes}")
+            y_offset = 30  
+
+            for i in present_classes:
+                print(i)
+                cv2.putText(image, i, (10, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
+                y_offset += 30  
         
-        cv2.putText(image, predicted_class, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        
+        print(predicted_class)
     else:
         print("LLLL")
         
@@ -93,24 +129,17 @@ def process_image(input_path, output_path, checked_value):
                 conf = box.conf[0]
                 cls = int(box.cls[0])
 
-                cv2.rectangle(resized_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-                label = f'{YoloModel.names[cls]} {conf:.2f}'
-                cv2.putText(resized_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+                label = f'{models[checked_value].names[cls]} {conf:.2f}'
+                cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+        # Save the result
+        cv2.imwrite(output_path, image)
+        print(f"Processed image saved at{output_path}")
 
     
 
-    # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-    
-    # edges = cv2.Canny(blurred_image, threshold1=100, threshold2=200)
-    
-    # contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # for contour in contours:
-    #     x, y, w, h = cv2.boundingRect(contour)
-        
-    #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+   
         
        
     
